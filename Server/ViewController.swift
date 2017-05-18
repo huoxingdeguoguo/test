dcbfd8ac41d1bc989f4a5649843649f229168b38
dulele <284872516@qq.com>
2017-05-18 15:53:33 +0800

    Initial Commit

---
+++ Server/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Server
+//
+//  Created by DLL on 17/5/18.
+//  Copyright © 2017年 DLL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
